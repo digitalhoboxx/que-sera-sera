8ef175aef984700dcec10d191c9e9c894d66212c
digitalhoboxx <73903490+digitalhoboxx@users.noreply.github.com>
2025-11-03 09:17:00 +0200

    initializr

---
+++ src/main/java/com/sh/que_sera_sera/QueSeraSeraApplication.java
@@ -0,0 +1,13 @@
+package com.sh.que_sera_sera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueSeraSeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueSeraSeraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sh/que_sera_sera/QueSeraSeraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.que_sera_sera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueSeraSeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
