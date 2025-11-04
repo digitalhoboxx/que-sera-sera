ab8b94229d0914b4789fe0c71cd013d501ca9678
digitalhoboxx <stanalin91@gmail.com>
2025-11-04 09:47:30 +0200

    update setup

--- src/main/java/com/sh/que_sera_sera/QueSeraSeraApplication.java
+++ src/main/java/com/sh/que_sera_sera/QueSeraSeraApplication.java
@@ -2,12 +2,20 @@ package com.sh.que_sera_sera;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class QueSeraSeraApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QueSeraSeraApplication.class, args);
+	}
+
+	@GetMapping
+	public String openingStatement() {
+		return "Welcome!";
 	}
 
 }
